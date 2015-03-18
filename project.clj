7639360c7aa6c7917ac8e12013d91cf2cec8872a
Chris Shea <cmshea@gmail.com>
2015-03-18 15:55:50 -0400

    v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/kehaar "0.1.0-SNAPSHOT"
+(defproject democracyworks/kehaar "0.1.0"
   :description "Kehaar passes messages to and from RabbitMQ channels"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
