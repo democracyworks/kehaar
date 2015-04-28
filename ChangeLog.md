8db52832f8ab5acf7b533d2d23a56bee35c1a6c7
Chris Shea <cmshea@gmail.com>
2015-04-28 18:25:09 -0400

    [CS] Tests using RabbitMQ

--- ChangeLog.md
+++ ChangeLog.md
@@ -8,3 +8,12 @@
 on. **This is a breaking change.** Queues must now be already
 declared. This allows, for example, the queue to be
 [a server-named, exclusive, auto-deleted queue](http://clojurerabbitmq.info/articles/queues.html#declaring-a-temporary-exclusive-queue).
+
+### Tests using RabbitMQ
+
+There are now tests which use RabbitMQ, however they are not run by
+default with `lein run`. In order to run the RabbitMQ tests, start
+`rabbitmq-server` with its default configuration and run `lein test
+:rabbit-mq`. To run all tests, run `lein test :all`.
+
+Travis CI has been updated to run those tests as well.
---
+++ test/kehaar/rabbit_mq_test.clj
@@ -0,0 +1,24 @@
+(ns kehaar.rabbit-mq-test
+  (:require [clojure.test :refer :all]
+            [kehaar :refer :all]
+            [clojure.core.async :as async]
+            [langohr.core :as rmq]
+            [langohr.channel :as lch]
+            [langohr.queue :as lq]
+            [langohr.basic :as lb]
+            [langohr.consumers :as lc]))
+
+(deftest ^:rabbit-mq async->rabbit-rabbit->async-test
+  (let [conn (rmq/connect)
+        ch (lch/open conn)
+        rabbit-queue (lq/declare-server-named ch {:exclusive true})
+        chan (async/chan)
+        response-chan (async/chan)
+        message {:test (java.util.UUID/randomUUID)}]
+    (async->rabbit chan ch rabbit-queue)
+    (lc/subscribe ch rabbit-queue (rabbit->async response-chan) {:auto-ack true})
+    (async/>!! chan message)
+    (is (= message (async/<!! response-chan)))
+
+    (rmq/close ch)
+    (rmq/close conn)))
