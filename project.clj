9e8121aaed6c215dc43e421c394acec387e8fd0e
Troy Hill <thill@snowfall.local>
2015-05-06 07:15:24 -0600

    [TH] upping to next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/kehaar "0.2.0"
+(defproject democracyworks/kehaar "0.2.1-SNAPSHOT"
   :description "Kehaar passes messages to and from RabbitMQ channels"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
