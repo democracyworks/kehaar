478a263ae40dc89baf1a2a55b2c51db71fd9ae70
Wes Morgan <cap10morgan@gmail.com>
2015-07-08 21:40:57 -0400

    [WM] update to clojure 1.7.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Kehaar passes messages to and from RabbitMQ channels"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.novemberain/langohr "3.2.0"]
                  [org.clojure/tools.logging "0.3.1"]]
