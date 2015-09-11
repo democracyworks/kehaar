5f52a99ae4776bbca43ae5a10505071c8f2ff567
Wes Morgan <cap10morgan@gmail.com>
2015-09-11 12:26:48 -0600

    [WM] fix alt!! put syntax in test helper

--- src/kehaar/test_helpers.clj
+++ src/kehaar/test_helpers.clj
@@ -5,7 +5,7 @@
   ([in-channel out-channel] (responder-test in-channel out-channel 10000))
   ([in-channel out-channel timeout]
    (fn [message]
-     (let [request-result (async/alt!! [in-channel {:message message}] ::sent
+     (let [request-result (async/alt!! [[in-channel {:message message}]] ::sent
                                        (async/timeout timeout) ::timeout)]
        (case request-result
          ::sent (:message (async/alt!! out-channel ([v] v)
