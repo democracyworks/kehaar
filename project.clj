df78765f6eb9bab0b80751373f6feb05140872d3
Chris Shea <cmshea@gmail.com>
2016-07-14 11:29:22 -0600

    Update clojure.core.async

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.385"]
                  [com.novemberain/langohr "3.2.0"]
                  [org.clojure/tools.logging "0.3.1"]]
   :test-selectors {:default (complement :rabbit-mq)
