891b3fa30ae7a88c4fbdad4c8236f773efebb1b0
Daniel Glauser <danglauser@gmail.com>
2016-09-22 14:58:27 -0600

    New async->fire-and-forget-fn that takes optional metadata

--- src/kehaar/wire_up.clj
+++ src/kehaar/wire_up.clj
@@ -240,3 +240,15 @@
     (let [response-channel (async/chan 1)]
       (async/>!! channel [response-channel message])
       response-channel)))
+
+(defn async->fire-and-forget-fn
+  "Returns a fn that takes a message, creates a core.async channel for
+  the response for that message, and puts message and metadata on the
+  channel. Returns the value of async/>!! which returns true if
+  it was successfull putting a message on the channel."
+  ([channel]
+   (async->fire-and-forget-fn channel {}))
+  ([channel metadata]
+   (fn [message]
+     (async/>!! channel {:metadata metadata
+                         :message message}))))
