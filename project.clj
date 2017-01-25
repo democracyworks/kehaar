9a6b67638c66b08de5a2aabe2e622039e5cdafc4
Christopher Shea <cmshea@gmail.com>
2017-01-25 15:34:05 -0500

    Version 0.9.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/kehaar "0.9.0"
+(defproject democracyworks/kehaar "0.9.1-SNAPSHOT"
   :url "https://github.com/democracyworks/kehaar"
   :description "Kehaar passes messages to and from RabbitMQ channels"
   :license {:name "Eclipse Public License"
