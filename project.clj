8578eafd55fe2e65cea94e07d5ce84798a266848
Wes Morgan <wesmorgan@icloud.com>
2017-06-10 08:58:17 -0600

    Use jitpack for paradise dependency for now

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :description "Kehaar passes messages to and from RabbitMQ channels"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.443"]
                  [com.novemberain/langohr "3.7.0"]
                  [org.apache.commons/commons-collections4 "4.1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [paradise "0.1.0-SNAPSHOT"]]
+                 [com.github.democracyworks/paradise "master-SNAPSHOT"]]
   :test-selectors {:default (complement :rabbit-mq)
                    :rabbit-mq :rabbit-mq
                    :all (constantly true)}
