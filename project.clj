fcc2fd18456a09377461b6616cb5f0af77dec137
Wes Morgan <wesmorgan@icloud.com>
2018-01-26 07:37:15 -0700

    Version 0.11.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/kehaar "0.11.4"
+(defproject democracyworks/kehaar "0.11.5-SNAPSHOT"
   :url "https://github.com/democracyworks/kehaar"
   :description "Kehaar passes messages to and from RabbitMQ channels"
   :license {:name "Eclipse Public License"
