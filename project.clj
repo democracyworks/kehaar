86c704432e4e8cc8b37162ab43014dbbb20dfa7f
Mike Richards <mrichards@democracy.works>
2019-06-14 11:14:11 -0400

    Version 1.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/kehaar "1.0.3"
+(defproject democracyworks/kehaar "1.0.4-SNAPSHOT"
   :url "https://github.com/democracyworks/kehaar"
   :description "Kehaar passes messages to and from RabbitMQ channels"
   :license {:name "Eclipse Public License"
